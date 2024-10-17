e70e06bdeb31f093b055727de431a81dd484b94d
dunglv202 <dunglv202@gmail.com>
2024-10-17 11:28:02 +0700

    require teacher code

--- src/main/java/dev/dunglv202/hoaithuong/dto/ConfigsDTO.java
+++ src/main/java/dev/dunglv202/hoaithuong/dto/ConfigsDTO.java
@@ -3,12 +3,14 @@ package dev.dunglv202.hoaithuong.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.dunglv202.hoaithuong.helper.SheetHelper;
 import dev.dunglv202.hoaithuong.validator.SpreadsheetURL;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ConfigsDTO {
+    @NotBlank(message = "{teacher.code.required}")
     private String teacherCode;
 
     private String calendarId;

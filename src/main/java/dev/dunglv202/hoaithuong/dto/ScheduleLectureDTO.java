95e62f2dd46c57862fa9b65829a01bcc4e0cfd5e
dunglv202 <dunglv202@gmail.com>
2024-12-19 09:33:06 +0700

    remove redundant dto fields

--- src/main/java/dev/dunglv202/hoaithuong/dto/ScheduleLectureDTO.java
+++ src/main/java/dev/dunglv202/hoaithuong/dto/ScheduleLectureDTO.java
@@ -7,8 +7,4 @@ import lombok.Setter;
 @Setter
 public class ScheduleLectureDTO {
     private long id;
-    private String topic;
-    private String video;
-    private String comment;
-    private String notes;
 }
